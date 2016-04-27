e0fd1996b35ca3687acb35cd0ec012cebe70b11b
Luis Larrea <luis@Luis-Larreas-MacBook-Pro.local>
2016-04-27 02:06:32 -0500

    Added cask script

---
+++ install/cask.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo -e "\n\nInstalling applications..."
+echo "=============================="
+
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install slack
+brew cask install flux
+brew cask install cinch
+brew cask install sizeup
